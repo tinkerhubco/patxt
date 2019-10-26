98e01423fb3218a5d8a0ec81a8bf17de9579981b
isotopeee <johnmichael.doroy@gmail.com>
2019-10-26 15:19:32 +0800

    feat: set up API Lerna package

---
+++ packages/patxt-api/src/index.ts
@@ -0,0 +1,16 @@
+import { PatextApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+
+export { PatextApplication };
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new PatextApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
