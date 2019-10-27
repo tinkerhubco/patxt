1a09155474226ddfb027438cfa97cd76f52bada9
isotopeee <johnmichael.doroy@gmail.com>
2019-10-27 17:43:34 +0800

    feat: create webhook to handle sending message via SMS
    Closes #5

--- packages/patxt-api/src/controllers/webhooks.controller.ts
+++ packages/patxt-api/src/controllers/webhooks.controller.ts
@@ -1,8 +1,11 @@
 import { repository } from '@loopback/repository';
-import { get, param } from '@loopback/rest';
+import { get, param, post, requestBody } from '@loopback/rest';
+import axios from 'axios';
 
 import { UserRepository } from '../repositories';
 import { formatToE164PhoneNumber } from '../utils';
+
+const GLOBE_APP_SHORT_CODE_SUFFIX = process.env.GLOBE_APP_SHORT_CODE_SUFFIX;
 
 export class WebhooksController {
   constructor(
@@ -11,14 +14,22 @@ export class WebhooksController {
 
   @get('/webhooks/globelabsapi/redirecturi', {
     parameters: [
-      { name: 'access_token', schema: { type: 'string' }, in: 'query' },
-      { name: 'subscriber_number', schema: { type: 'string' }, in: 'query' },
+      {
+        name: 'access_token',
+        schema: { type: 'string' },
+        in: 'query',
+      },
+      {
+        name: 'subscriber_number',
+        schema: { type: 'string' },
+        in: 'query',
+      },
     ],
     responses: {
-      '200': {
+      '201': {
         description: 'Globe Labs API - Redirect URI webhook response (Opt-in)',
         content: {
-          'appication/json': {
+          'application/json': {
             schema: {
               type: 'object',
               properties: {
@@ -44,6 +55,135 @@ export class WebhooksController {
     });
 
     console.log('Created user record: ', newUserRecord);
+
+    return {
+      success: true,
+    };
+  }
+
+  @post('/webhooks/globelabsapi/notifyuri', {
+    parameters: [
+      { name: 'access_token', schema: { type: 'string' }, in: 'query' },
+    ],
+
+    responses: {
+      '201': {
+        description: 'Globe Labs API - Notify URI webhook response',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                success: { type: 'boolean' },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  async notifyUriWebhook(@requestBody({
+    description: 'data',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            inboundSMSMessageList: {
+              type: 'object',
+              properties: {
+                inboundSMSMessage: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      dateTime: { type: 'string' },
+                      destinationAddress: { type: 'string' },
+                      messageId: { nullable: true, type: 'string' },
+                      message: { type: 'string' },
+                      resourceURL: { nullable: true, type: 'string' },
+                      senderAddress: { type: 'string' },
+                    },
+                  },
+                },
+                numberOfMessagesInThisBatch: { type: 'number' },
+                resourceURL: { nullable: true, type: 'string' },
+                totalNumberOfPendingMessages: {
+                  nullable: true,
+                  type: 'string',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    required: true,
+  })
+  requestBody: {
+    inboundSMSMessageList: {
+      inboundSMSMessage: Array<{
+        dateTime: string; // Fri Nov 22 2013 12:12:13 GMT+0000 (UTC)
+        destinationAddress: string; // tel:21581234
+        messageId: string | null;
+        message: string;
+        resourceURL: string | null;
+        senderAddress: string; // tel:+639171234567
+      }>;
+      numberOfMessagesInThisBatch: number;
+      resourceURL: string | null;
+      totalNumberofPendingMessages: number | null;
+    };
+  }) {
+    const {
+      inboundSMSMessageList: { inboundSMSMessage },
+    } = requestBody;
+
+    await Promise.all(
+      inboundSMSMessage.map(async ({ message: inbountMessage }) => {
+        const [
+          action,
+          recipientPhoneNumber,
+          ...messageParts
+        ] = inbountMessage.split(' ');
+
+        const formattedDestinationPhoneNumber = formatToE164PhoneNumber(
+          recipientPhoneNumber,
+        );
+
+        const recipientUser = await this.userRepository.findOne({
+          where: {
+            phoneNumber: formattedDestinationPhoneNumber,
+          },
+        });
+
+        if (action.toLowerCase() === 'send') {
+          if (!recipientUser) {
+            // TODO: Handle gracefully
+            return;
+          }
+
+          const { smsAccessToken } = recipientUser;
+
+          await axios.post(
+            `https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/${GLOBE_APP_SHORT_CODE_SUFFIX}/requests?access_token=${smsAccessToken}`,
+            {
+              outboundSMSMessageRequest: {
+                address: formattedDestinationPhoneNumber,
+                // TODO: Should be auto-generated and unique for each message
+                clientCorrelator: '123456',
+                outboundSMSTextMessage: { message: messageParts.join(' ') },
+                senderAddress: GLOBE_APP_SHORT_CODE_SUFFIX,
+              },
+            },
+          );
+
+          console.log('Message sent to: ', formattedDestinationPhoneNumber);
+
+          // TODO: Log/Save message sent
+        }
+      }),
+    );
 
     return {
       success: true,
