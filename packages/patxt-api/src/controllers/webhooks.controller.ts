ad5813103eb03c5bcdbadf7d92b636f01897849d
isotopeee <johnmichael.doroy@gmail.com>
2019-10-27 20:06:14 +0800

    feat: create webhook to handle sending message via Facebook messenger

--- packages/patxt-api/src/controllers/webhooks.controller.ts
+++ packages/patxt-api/src/controllers/webhooks.controller.ts
@@ -189,4 +189,97 @@ export class WebhooksController {
       success: true,
     };
   }
+
+  @get('/webhooks/messenger/callback')
+  async messengerWebhookGet(
+    @param.query.string('hub.mode') mode: string,
+    @param.query.string('hub.verify_token') token: string,
+    @param.query.string('hub.challenge') challenge: string,
+  ) {
+    console.log('GET');
+
+    console.log(mode);
+    console.log(token);
+    console.log(challenge);
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = 'test';
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        // res.status(200).send(challenge);
+        return challenge;
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        // res.sendStatus(403);
+        return {
+          success: false,
+        };
+      }
+    }
+  }
+
+  @post('/webhooks/messenger/callback')
+  async messengerWebhookPost(@requestBody()
+  requestBody: {
+    object: string;
+    entry: Array<{
+      id: string;
+      time: number;
+      messaging: Array<{
+        sender: { id: string };
+        recipient: { id: string };
+        timestamp: number;
+        message: { mid: string; text: string };
+      }>;
+    }>;
+  }) {
+    const [
+      action,
+      recipientPhoneNumber,
+      ...messageParts
+    ] = requestBody.entry[0].messaging[0].message.text.split(' ');
+
+    const formattedDestinationPhoneNumber = formatToE164PhoneNumber(
+      recipientPhoneNumber,
+    );
+
+    const recipientUser = await this.userRepository.findOne({
+      where: {
+        phoneNumber: formattedDestinationPhoneNumber,
+      },
+    });
+
+    if (action.toLowerCase() === 'send') {
+      if (!recipientUser) {
+        // TODO: Handle gracefully
+        return;
+      }
+
+      const { smsAccessToken } = recipientUser;
+
+      await axios.post(
+        `https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/${GLOBE_APP_SHORT_CODE_SUFFIX}/requests?access_token=${smsAccessToken}`,
+        {
+          outboundSMSMessageRequest: {
+            address: formattedDestinationPhoneNumber,
+            // TODO: Should be auto-generated and unique for each message
+            clientCorrelator: '123456',
+            outboundSMSTextMessage: { message: messageParts.join(' ') },
+            senderAddress: GLOBE_APP_SHORT_CODE_SUFFIX,
+          },
+        },
+      );
+
+      console.log('Message sent to: ', formattedDestinationPhoneNumber);
+
+      // TODO: Log/Save message sent
+    }
+
+    // console.log(requestBody.entry[0].messaging[0].message.text);
+  }
 }
